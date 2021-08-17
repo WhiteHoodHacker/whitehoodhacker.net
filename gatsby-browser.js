81049fd49bec9ac72756cf46e68a6e8f7c391083
WhiteHoodHacker <github@whitehoodhacker.net>
2021-08-17 11:11:35 -0500

    Remove re-render function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,12 +10,3 @@ import "prismjs/themes/prism-okaidia.css"
 
 import customWrapPageElement from "./src/components/wrap-page-element"
 export const wrapPageElement = customWrapPageElement
-
-// this is a hack to fix missing styles on refresh in production
-// reference: https://github.com/gatsbyjs/gatsby/issues/17676#issuecomment-535796737
-import ReactDOM from "react-dom"
-export function replaceHydrateFunction() {
-    return (element, container, callback) => {
-        ReactDOM.render(element, container, callback);
-    };
-}
